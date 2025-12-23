50e897c607bf5f345f04db4d73eb7ac5812f8a26
YvnPretty <yvngmolly00@gmail.com>
2025-12-23 16:03:25 -0600

    fix: use standard base path for gh-pages

---
+++ src/App_test_failed.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div style={{ background: '#050507', color: 'white', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <h1>PDF Master - Test Render</h1>
+        </div>
+    );
+};
+
+export default App;
